a993f0cc85750080913049249bfc587ae1c7f7b9
SheetalWaldekar <sheetalwaldekar@gmail.com>
2026-08-24 20:05:25 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,124 @@
+##PBMC3K Single-Cell RNA-seq Analysis with R and Seurat
+
+##Project Overview
+
+This project performs an end-to-end single-cell RNA sequencing (scRNA-seq) analysis of the publicly available PBMC3K dataset from 10x Genomics.
+
+The dataset contains approximately 2,700 human peripheral blood mononuclear cells (PBMCs) and was generated using 10x Genomics technology.
+
+##Objective
+
+The goal is to process the expression matrix, identify transcriptionally distinct cell populations, visualize the cells, identify cluster-specific marker genes, and annotate the clusters using known immune-cell markers.
+
+##Biological Question
+
+What major immune cell populations can be identified from the PBMC3K single-cell RNA-seq dataset based on their gene-expression profiles?
+
+##Dataset
+
+Dataset: 10x Genomics PBMC3K
+Organism: Homo Sapians
+Initial Cells: 2700
+Initial genes: 32738
+Technology: 10x Genomics single-cell RNA-seq
+Genome annotation in the supplied matrix: hg19
+Download source: 10x Genomics PBMC3K dataset
+
+The raw 10x matrix is intentionally not required in this GitHub repository. It can be downloaded from the original source using the shell script.
+
+##Tools and Technologies
+
+Linux / Ubuntu
+R 
+Seurat
+ggplot2
+dplyr
+patchwork
+PCA 
+UMAP
+Marker-gene analysis
+
+##Analysis Workflow
+
+10x Genomics expression matrix
+           ↓
+     Data loading
+           ↓
+     Create Seurat Object
+           ↓
+     Quality control
+           ↓
+       Cell/gene filtering
+           ↓
+       Normalization
+           ↓
+ Highly variable gene selection
+           ↓
+       Scale Data
+           ↓
+          PCA
+           ↓
+  Nearest-neighbor graph
+           ↓
+      clustering
+           ↓
+         UMAP 
+           ↓         
+   Marker gene analysis
+           ↓
+   Cell-type annotation
+           ↓
+   Biological Interpritation
+
+##Result
+
+After quality-control filtering, 2640 cells remained from the original PBMC3K dataset of approximately 2700 cells and 13714 genes remained from aproximately 32738.
+PCA-based dimensionality reduction followed by graph-based clustering identified 9 transcriptionally distinct cell clusters.
+The clusters contained 684, 481, 476, 344, 291, 162, 155, 32, and 13 cells, respectively.
+Cell-type annotation identified nine distinct populations: naive CD4 T cells, memory CD4 T cells, CD8 T cells, CD14+ classical monocytes, FCGR3A+ monocytes, B cells, NK cells, dendritic cells, and platelets. These populations represent major lymphoid, myeloid, antigen-presenting, and platelet lineages present in peripheral blood.
+
+Cluster	Cell type:
+Cluster 0         Naive CD4 T
+Cluster 1	CD14+ Monocytes
+Cluster 2	Memory CD4 T
+Cluster 3	B cells
+Cluster 4	CD8 T
+Cluster 5	FCGR3A+ Monocytes
+Cluster 6	NK cells
+Cluster 7	Dendritic cells
+Cluster 8	Platelets
+
+##Conclusion
+
+This project demonstrates a complete scRNA-seq analysis workflow from a raw 10x Genomics expression matrix to biologically interpretable cell populations.
+The analysis demonstrates practical skills in:
+Single-cell RNA-seq preprocessing through Quality control, Normalization, Dimensionality reduction, Clustering, Marker gene analysis, Cell-type annotation, Biological interpretation, Reproducible computational analysis
+
+##Major Cell-Type Markers
+
+T cells: CD3D, CD3E
+B cells: MS4A1, CD79A, CD79B, CD37
+NK cells: NKG7, GNLY, PRF1, GZMA, GZMM, CCL5
+Monocytes: LYZ, LST1, TYROBP, AIF1, FCN1, S100A8, S100A9
+Dendritic cells: FCER1A, CST3, CD74, HLA-DRA, HLA-DPA1, HLA-DPB1
+Platelet/Megakaryocyte-like cells: PF4, PPBP, RGS18, SDPR
+
+##Visualization and Interpretation
+
+**QC Violin Plot:** Shows the distribution of genes detected per cell, total RNA counts per cell, and mitochondrial RNA percentage.
+
+**PCA Plot:** Shows the major sources of transcriptional variation captured by principal components.
+
+**Elbow Plot:** Helps determine the number of principal components used for downstream analysis.
+
+**UMAP Cluster Plot:** Displays computationally identified cell populations. Each point represents a cell.
+
+**Marker DotPlot:** Compares marker-gene expression across clusters. Dot size represents the percentage of cells expressing the gene, while dot intensity represents average expression.
+
+**FeaturePlot:** Shows where specific marker genes are expressed on the UMAP.
+
+
+##Author
+
+Sheetal
+
---
+++ SCRNA.R
@@ -0,0 +1,222 @@
+install.packages("Seurat")
+install.packages(c("ggplot2", "dplyr", "patchwork"))
+library(Seurat)
+library(ggplot2)
+library(dplyr)
+library(patchwork)
+packageVersion("Seurat")
+packageVersion("ggplot2")
+packageVersion("dplyr")
+packageVersion("patchwork")
+
+## Load data
+pbmc3k_data <- Read10X(
+  data.dir= "//wsl.localhost/Ubuntu-24.04/home/shital/my_project/scrna_project/filtered_gene_bc_matrices/hg19"
+)
+list.files("//wsl.localhost/Ubuntu-24.04/home/shital/my_project/scrna_project/filtered_gene_bc_matrices/hg19")
+dim (pbmc3k_data)
+
+## create seurat Object
+pbmc <- CreateSeuratObject(counts = pbmc3k_data,
+                           project = 'PBMC3K',
+                           min.cells = 3,
+                           min.features = 200)
+pbmc
+ncol(pbmc)
+nrow(pbmc)
+
+# QC
+# mitochondrial % calculate
+pbmc[['mt_percent']] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
+View(pbmc@meta.data)
+
+#Visualize QC Matrix
+VlnPlot(pbmc, features = c('nCount_RNA', 'nFeature_RNA', 'mt_percent' ))
+
+#Counts vs Mitochondrial %
+plot1<- FeatureScatter(pbmc, feature1='nCount_RNA', feature2='mt_percent')
+
+# Counts vs genes
+plot2<- FeatureScatter(pbmc, feature1='nCount_RNA', feature2='nFeature_RNA')
+plot1 + plot2
+
+# Filter Low quality cells
+pbmc <- subset(pbmc,
+               subset= nFeature_RNA > 200 &
+                 nFeature_RNA < 2500 &
+                 mt_percent < 5)
+pbmc
+
+#Normalization and feature selection
+pbmc <- NormalizeData(pbmc,
+                      normalization.method = "LogNormalize",
+                      scale.factor = 10000
+)
+pbmc
+
+# find highly variable genes
+pbmc <- FindVariableFeatures(
+  pbmc,
+  selection.method = 'vst',
+  nfeatures = 2000
+)
+top10 <- head(VariableFeatures(pbmc), 10)
+
+plot1 <- VariableFeaturePlot(pbmc)
+
+plot2 <- LabelPoints(
+  plot = plot1,
+  points = top10,
+  repel = TRUE
+)
+plot2
+
+# Scale Data (prepare Data for PCA plot)
+all.genes <- rownames(pbmc)
+pbmc <- ScaleData(pbmc, features = all.genes)
+pbmc
+
+#PCA plot
+pbmc <- RunPCA(pbmc, features = VariableFeatures(pbmc))
+print(pbmc[["pca"]], dims = 1:5)
+
+#visualize top genes driving each pc
+VizDimLoadings(pbmc, dims=1:2, reduction='pca')
+
+DimPlot(
+  pbmc,
+  reduction = "pca"
+)
+
+# Elbow plot to Decide how many PCs to use
+ElbowPlot(pbmc)
+
+#find Nearest neighbors
+pbmc <- FindNeighbors(pbmc, dims = 1:10)
+pbmc
+
+#Clustering
+pbmc <- FindClusters(pbmc,resolution = 0.5)
+print(paste("Number of clusters:", length(levels(pbmc))))
+
+table(Idents(pbmc))
+
+#UMAP
+pbmc <- RunUMAP(pbmc, dims = 1:10)
+
+#first major biological figure
+DimPlot(pbmc, reduction = 'umap', label=TRUE)
+
+# Find markers for every cluster vs all others
+pbmc_markers <- FindAllMarkers(
+  pbmc,
+  only.pos = TRUE,
+  min.pct = 0.25,
+  logfc.threshold = 0.25
+)
+View(pbmc_markers)
+
+# Top markers per cluster
+pbmc_markers %>%
+  group_by(cluster) %>%
+  slice_max(n=2, order_by=avg_log2FC)
+
+# Heatmap of top 10 markers per cluster
+top10 <- pbmc_markers %>%
+  group_by(cluster) %>%
+  slice_max(n=10, order_by=avg_log2FC)
+top10
+DoHeatmap(pbmc, features=top10$gene) + NoLegend()
+
+# DotPlot Visualize marker genes 
+markers_to_plot <- c(
+  "CD3D",
+  "CD3E",
+  "MS4A1",
+  "CD79A",
+  "NKG7",
+  "GNLY",
+  "LYZ",
+  "LST1",
+  "FCER1A",
+  "CST3",
+  "PF4",
+  "PPBP"
+)
+
+DotPlot(
+  pbmc,
+  features = markers_to_plot
+) +
+  RotatedAxis()
+
+# Visualize known markers
+FeaturePlot(pbmc,
+            features=c('CD3D','CD4','CD8A','MS4A1',
+                       'GNLY','CD14','LYZ','FCGR3A'),
+            ncol=3)
+
+# Assign cell type labels (adjust cluster numbers to match yours)
+new_cluster_ids <- c('Naive CD4 T', 'CD14+ Mono', 'Memory CD4 T',
+                     'B', 'CD8 T', 'FCGR3A+ Mono', 'NK',
+                     'DC', 'Platelet')
+new_cluster_ids
+names(new_cluster_ids) <- levels(pbmc)
+pbmc <- RenameIdents(pbmc, new_cluster_ids)
+
+# Final annotated UMAP
+DimPlot(pbmc, reduction='umap', label=TRUE, pt.size=0.5) + NoLegend()
+
+#Compare CD14 Monocytes vs CD16 Monocytes
+mono.markers <- FindMarkers(pbmc,
+                            ident.1='CD14+ Mono',
+                            ident.2='FCGR3A+ Mono',
+                            min.pct=0.25)
+head(mono.markers, 10) 
+
+saveRDS(pbmc, file='pbmc3k_analyzed.rds')
+pbmc <- readRDS('pbmc3k_analyzed.rds')
+
+write.csv(Idents(pbmc), file='cell_type_assignments.csv')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
